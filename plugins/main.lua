e0a2cfe0874064d3f78de5bfdd3a5e30a23e6638
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-07-31 12:47:53 -0400

    commit

--- plugins/main.lua
+++ plugins/main.lua
@@ -1,7 +1,6 @@
 -- RoRR Modding Toolkit v1.0.0
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
---mods.on_all_mods_loaded(function() for k, v in pairs(mods) do if type(v) == "table" and v.hfuncs then Helper = v end end end)
 
 RoRR_Modding_Toolkit = true
 
@@ -9,8 +8,3 @@ require("instance")
 require("item")
 require("net")
 require("player")
-
-
-
--- ========== Main ==========
-
