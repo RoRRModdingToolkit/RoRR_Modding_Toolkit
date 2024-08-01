32f0f835979eb81a303114dc7c8519c909f60a0d
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-08-01 14:23:47 -0400

    Update instance.lua

--- plugins/instance.lua
+++ plugins/instance.lua
@@ -61,3 +61,83 @@ Instance.find_all = function(...)
 
     return insts, #insts > 0
 end
+
+
+Instance.spawn_crate = function(x, y, rarity, items, depth)
+    local lang_map = gm.variable_global_get("_language_map")
+    local class_item = gm.variable_global_get("class_item")
+
+    local sprites = {gm.constants.sCommandCrateCommon, gm.constants.sCommandCrateUncommon, gm.constants.sCommandCrateRare, gm.constants.sCommandCrateEquipment, gm.constants.sCommandCrateBoss}
+    local sprites_use = {gm.constants.sCommandCrateCommonUse, gm.constants.sCommandCrateUncommonUse, gm.constants.sCommandCrateRareUse, gm.constants.sCommandCrateEquipmentUse, gm.constants.sCommandCrateBossUse}
+    local isi = {6.0, 8.0, 10.0, 14.0, 12.0}
+    local isii = {5.0, 7.0, 9.0, 13.0, 11.0}
+
+    -- Move downwards until on the ground
+    while not gm.position_meeting(x, y, gm.constants.pBlockStatic) and y < gm.variable_global_get("room_height") do y = y + 1 end
+
+    -- Taken from Scrappers mod
+    local c = gm.instance_create_depth(x, y, depth or 0, gm.constants.oCustomObject_pInteractableCrate)
+
+    -- Most of the following are necessary,
+    -- and are not set from creating the instance directly (via gm.instance_create)
+    c.active = 0.0
+    c.owner = -4.0
+    c.activator = -4.0
+    c.buy_button_visible = 0.0
+    c.can_activate_frame = 0.0
+    c.mouse_x_last = 0.0
+    c.mouse_y_last = 0.0
+    c.last_move_was_mouse = false
+    c.using_mouse = false
+    c.last_activated_frame = -1.0
+    c.cam_rect_x1 = x - 100
+    c.cam_rect_y1 = y - 100
+    c.cam_rect_x2 = x + 100
+    c.cam_rect_y2 = y + 100
+    c.contents = nil
+    c.inventory = 74.0 + (rarity * 2.0)
+    c.flash = 0.0
+    c.interact_scroll_index = isi[rarity]
+    c.interact_scroll_index_inactive = isii[rarity]
+    c.surf_text_cost_large = -1.0
+    c.surf_text_cost_small = -1.0
+    c.translation_key = "interactable.pInteractableCrate"
+    c.text = gm.ds_map_find_value(lang_map, c.translation_key..".text")
+    c.spawned = true
+    c.cost = 0.0
+    c.cost_color = 8114927.0
+    c.cost_type = 0.0
+    c.selection = 0.0
+    c.select_cd = 0.0
+    c.sprite_index = sprites[rarity]
+    c.sprite_death = sprites_use[rarity]
+    c.fade_alpha = 0.0
+    c.col_index = rarity - 1.0
+    c.m_id = gm.set_m_id(true)  -- I have no idea what the argument is supposed to be, but this works
+    c.my_player = -4.0
+    c.__custom_id = rarity - 1.0
+    c.__object_index = 799.0 + rarity
+    c.image_speed = 0.06
+    c.tier = rarity - 1.0
+
+    -- Replace default crate items with custom set
+    if items then
+        c.contents = gm.array_create()
+        for _, i in ipairs(items) do
+            gm.array_push(c.contents, class_item[i + 1][9])
+        end
+    end
+
+    -- [Host]  Send spawn data to clients
+    if Net.type() == Net.TYPE.host then Net.send("RMT.spawnCrate", x, y, rarity, items, depth) end
+
+    return c
+end
+
+
+
+-- ========== Internal ==========
+
+mods.on_all_mods_loaded(function()
+    Net.register("RMT.spawnCrate", Instance.spawn_crate)
+end)
