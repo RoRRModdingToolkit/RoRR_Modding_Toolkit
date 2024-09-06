c5d7b579a9a7783f42094786943890adcb52ac1b
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-09-06 01:39:19 -0400

    Update wrap.lua

--- plugins/wrap.lua
+++ plugins/wrap.lua
@@ -13,7 +13,7 @@ Wrap.wrap = function(value)
     end
 
     -- List
-    -- NOTE: Can't really wrap this since it's just an index number,
+    -- NOTE: Can't really wrap this automatically since it's just an index number,
     -- meaning that normal integer variables can get caught in the wrap as well
     -- if gm.ds_exists(value, 1) then  -- 1 is "ds_type_list"
     --     return List.wrap(value)
