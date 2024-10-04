17787480dd3d2769e1f6976c9538b86aa41110c2
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-03 23:44:07 -0400

    Update gm.lua

--- plugins/gm.lua
+++ plugins/gm.lua
@@ -39,12 +39,14 @@ gm_add_instance_methods = function(methods_table)
     for fn, _ in pairs(gm.constants) do
         local type_ = gm.constant_types[fn]
         if type_ == "script" or type_ == "gml_script" then
-            methods_table[fn] = function(self, ...)
-                local t = {...}
-                for i, arg in ipairs(t) do
-                    t[i] = Wrap.unwrap(arg)
+            if not methods_table[fn] then
+                methods_table[fn] = function(self, ...)
+                    local t = {...}
+                    for i, arg in ipairs(t) do
+                        t[i] = Wrap.unwrap(arg)
+                    end
+                    return gm.call(fn, self.value, self.value, table.unpack(t))
                 end
-                return gm.call(fn, self.value, self.value, table.unpack(t))
             end
         end
     end
