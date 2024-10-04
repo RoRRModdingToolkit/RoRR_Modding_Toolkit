246cfda509b050f25e276dcc99a7c3cf3d669c18
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-03 20:51:08 -0400

    Update gm.lua

--- plugins/gm.lua
+++ plugins/gm.lua
@@ -22,16 +22,18 @@ local functions = {
 -- ========== Hooks ==========
 
 for _, fn in ipairs(functions) do
-    gm.pre_script_hook(gm.constants[fn], function(self, other, result, args)
-        -- Check if function was run by a mod
-        -- (This is done by the fact that debug.getinfo(2) will be nil if run by the game)
-        -- If so, do Wrap.unwrap on all arguments
-        if debug.getinfo(2) then
-            self = Wrap.unwrap(self)
-            other = Wrap.unwrap(other)
-            for _, a in ipairs(args) do
-                a.value = Wrap.unwrap(a.value)
+    if gm.constants[fn] then
+        gm.pre_script_hook(gm.constants[fn], function(self, other, result, args)
+            -- Check if function was run by a mod
+            -- (This is done by the fact that debug.getinfo(2) will be nil if run by the game)
+            -- If so, do Wrap.unwrap on all arguments
+            if debug.getinfo(2) then
+                self = Wrap.unwrap(self)
+                other = Wrap.unwrap(other)
+                for _, a in ipairs(args) do
+                    a.value = Wrap.unwrap(a.value)
+                end
             end
-        end
-    end)
+        end)
+    end
 end
