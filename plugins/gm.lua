9ab5ff33397074a1c329093daf8b79df88587abe
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-03 20:50:13 -0400

    Update gm.lua

--- plugins/gm.lua
+++ plugins/gm.lua
@@ -4,7 +4,17 @@
 -- wrapped values will be automatically unwrapped
 
 local functions = {
-    gm.constants.recalculate_stats
+    "instance_create",
+    "recalculate_stats",
+    "skill_util_unlock_cooldown",
+    "skill_util_lock_cooldown",
+    "skill_util_facing_direction",
+    "skill_util_apply_friction",
+    "skill_util_exit_state_on_anim_end",
+    "actor_skill_add_stock",
+    "sound_play_at",
+    "",
+    "",
 }
 
 
@@ -12,7 +22,7 @@ local functions = {
 -- ========== Hooks ==========
 
 for _, fn in ipairs(functions) do
-    gm.pre_script_hook(fn, function(self, other, result, args)
+    gm.pre_script_hook(gm.constants[fn], function(self, other, result, args)
         -- Check if function was run by a mod
         -- (This is done by the fact that debug.getinfo(2) will be nil if run by the game)
         -- If so, do Wrap.unwrap on all arguments
