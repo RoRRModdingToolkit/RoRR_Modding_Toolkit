ce9e33dd2f8ede7b8e10222d045d9e35c37a96ec
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-08 13:44:49 -0400

    Update initialize.lua

--- plugins/initialize.lua
+++ plugins/initialize.lua
@@ -16,14 +16,14 @@ gm.pre_script_hook(gm.constants.__input_system_tick, function()
         -- Loop through all mods and call their __initialize functions
         for _, m in pairs(mods) do
             if type(m) == "table" and m.__initialize and (not m.RoRR_Modding_Toolkit) then
-                m.__initialize()
+                pcall(m.__initialize)
             end
         end
 
         -- Loop through all mods and call their __post_initialize functions
         for _, m in pairs(mods) do
             if type(m) == "table" and m.__post_initialize and (not m.RoRR_Modding_Toolkit) then
-                m.__post_initialize()
+                pcall(m.__post_initialize)
             end
         end
     end
