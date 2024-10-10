8ff0871ddf8503ee66a4dee3bd14c47c8cb375b3
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-10 10:52:14 -0400

    Update initialize.lua

--- plugins/initialize.lua
+++ plugins/initialize.lua
@@ -6,6 +6,20 @@ local init = false
 
 -- ========== Functions ==========
 
+local rmt_class_init = function(m)
+    if not pcall(function()
+        if not m.RMT_Class_Init then
+            m.RMT_Class_Init = true
+            for _, c in ipairs(Classes) do
+                if _G[c] then m._G[c] = _G[c] end
+            end
+        end
+    end) then
+        log.info("[!] "..m["!guid"].." : Failed to add RMT class references.")
+    end
+end
+
+
 gm.pre_script_hook(gm.constants.__input_system_tick, function()
     if not init then
         init = true
@@ -16,13 +30,7 @@ gm.pre_script_hook(gm.constants.__input_system_tick, function()
         -- Loop through all mods and call their __initialize functions
         for _, m in pairs(mods) do
             if type(m) == "table" and m.__initialize and (not m.RoRR_Modding_Toolkit) then
-                if not pcall(function()
-                    for _, c in ipairs(Classes) do
-                        if _G[c] then m._G[c] = _G[c] end
-                    end
-                end) then
-                    log.info("[!] "..m["!guid"].." : Failed to add RMT class references.")
-                end
+                rmt_class_init(m)
 
                 if not pcall(m.__initialize) then
                     log.info("[!] "..m["!guid"].." : __initialize failed to run.")
@@ -33,6 +41,8 @@ gm.pre_script_hook(gm.constants.__input_system_tick, function()
         -- Loop through all mods and call their __post_initialize functions
         for _, m in pairs(mods) do
             if type(m) == "table" and m.__post_initialize and (not m.RoRR_Modding_Toolkit) then
+                rmt_class_init(m)
+
                 if not pcall(m.__post_initialize) then
                     log.info("[!] "..m["!guid"].." : __post_initialize failed to run.")
                 end
