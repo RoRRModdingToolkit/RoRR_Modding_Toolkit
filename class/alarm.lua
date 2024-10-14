141623a408dca88d8fa06abae2c6a8e26717bfb1
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-10-14 16:36:33 -0400

    Update alarm.lua

--- class/alarm.lua
+++ class/alarm.lua
@@ -9,7 +9,6 @@ local current_frame = 0
 --
 
 Alarm.create = function(func, time, ...)
-    if not current_frame then return false end
     local future_frame = current_frame+time
     if not alarms[future_frame] then alarms[future_frame] = {} end
     alarms[future_frame][#alarms[future_frame]+1] = {
@@ -20,9 +19,8 @@ Alarm.create = function(func, time, ...)
 end
 
 gm.post_script_hook(gm.constants.__input_system_tick, function()
-    local new_current_frame = gm.variable_global_get("_current_frame")
-    if new_current_frame < current_frame then alarms = {} end   -- "_current_frame" resets on run start
-    current_frame = new_current_frame
+    -- "_current_frame" resets on run start, so using own counter now
+    current_frame = current_frame + 1
     if not alarms[current_frame] then return end
     for i=1, #alarms[current_frame] do
         local status, err = pcall(alarms[current_frame][i].fn, alarms[current_frame][i].args)
