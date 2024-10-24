ef4c60618cb75d854ca715885eb2df8d97474c2b
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-10-24 15:18:14 +0200

    Jam v0.0.0 Init

---
+++ _Examples/Survivor/README.md
@@ -0,0 +1,15 @@
+Example survivor for the RoRR Modding Toolkit
+
+Documentation can be found [here](https://github.com/RoRRModdingToolkit/RoRR_Modding_Toolkit/wiki).
+
+The source code can be found [here](https://github.com/RoRRModdingToolkit/RoRR_Modding_Toolkit/tree/Gabriel/_Examples/Survivor)
+
+This example shows you how to set up your custom survivor.
+
+If you have any additional questions, be sure to reach out in the [Discord](https://discord.gg/VjS57cszMq).
+
+---
+
+### Installation Instructions
+Install through the Thunderstore client or r2modman [(more detailed instructions here if needed)](https://return-of-modding.github.io/ModdingWiki/Playing/Getting-Started/).  
+Join the [Return of Modding server](https://discord.gg/VjS57cszMq) for support.  
--- _Examples/Survivor/jam.lua
+++ _Examples/Survivor/jam.lua
@@ -1,7 +1,5 @@
 -- RMTtest v1.0.0
--- RoRRModdingToolkit
-
-
+-- RoRRModdingToolkit - Umigatari/Miguel
 
 log.info("Successfully loaded ".._ENV["!guid"]..".")
 mods["RoRRModdingToolkit-RoRR_Modding_Toolkit"].auto() -- Loading the toolkit 
@@ -10,7 +8,6 @@ local PATH = _ENV["!plugins_mod_folder_path"]
 local NAMESPACE = "RMT"
 
 local initialize = function()
-
     local jam = Survivor.new(NAMESPACE, "jamman") -- Create a survivor 
 
     --[[------------------------------------------
@@ -50,7 +47,7 @@ local initialize = function()
     local sprPalette = Resources.sprite_load(NAMESPACE, "jam_palette", path.combine(PATH, "jam", "palette.png"))
     local sprSelectPalette = Resources.sprite_load(NAMESPACE, "sSelectJammanPalette", path.combine(PATH, "jam", "sSelectJammanPalette.png"))
     
-    -- The color of the character's skill names in the character select
+    -- The rgb color of the character's skill names in the character select
     jam:set_primary_color(Color.from_rgb(162, 62, 224))
     
     -- The character's sprite in the selection pod
@@ -70,6 +67,7 @@ local initialize = function()
     local jamman_portraitsmall_PAL1 = Resources.sprite_load(NAMESPACE, "sJammanPortraitSmall_PAL1", path.combine(PATH, "jam", "sJammanPortraitSmall_PAL1.png"))
     jam:add_skin("jammanpurple", 1, jamman_loadout_PAL1, jamman_portrait_PAL1, jamman_portraitsmall_PAL1)
     
+    -- Create second alternative skin
     local jamman_loadout_PAL2 = Resources.sprite_load(NAMESPACE, "sSelectJamman_PAL2", path.combine(PATH, "jam", "sSelectJamman_PAL2.png"), 4, 28, 0)
     local jamman_portrait_PAL2 = Resources.sprite_load(NAMESPACE, "sJammanPortrait_PAL2", path.combine(PATH, "jam", "sJammanPortrait_PAL2.png"), 3)
     local jamman_portraitsmall_PAL2 = Resources.sprite_load(NAMESPACE, "sJammanPortraitSmall_PAL2", path.combine(PATH, "jam", "sJammanPortraitSmall_PAL2.png"))
@@ -83,21 +81,32 @@ local initialize = function()
     jam:set_animations(sprites) -- Set the player's sprites to those we previously loaded
 
     --[[------------------------------------------
+        SECTION SURVIVOR LOG
+    ------------------------------------------]]--
+
+    --  create a new survivor log with the portrait sprite of the survivor as well as the log art
+    local log = Survivor_Log.new(jam, jam.sprite_portrait, sprites.jump)
+
+    -- All the log text is in the /elanguage/english.json and is loaded automatically
+
+    --[[------------------------------------------
         SECTION STATS
     ------------------------------------------]]--
 
     jam:set_stats_base({ -- Set the player's starting stats
         maxhp = 120,
         damage = 14,
-        regen = 0.01
+        regen = 0.01 -- health regen per frame, so 0.6 per second
     })
     
     jam:set_stats_level({  -- Set the player's leveling stats
         maxhp = 24,
-        damage = 2,
-        regen = 0.002,
+        damage = 4,
+        regen = 0.002, -- gain 0.12 health regen per level
         armor = 4
     })
+
+    
 
     --[[------------------------------------------
         SECTION SKILLS
@@ -129,7 +138,7 @@ local initialize = function()
     skill_spikes:set_skill_animation(sprShoot4)
     skill_spikesScepter:set_skill_animation(sprShoot4)
 
-    -- Create State skill
+    -- Create 1 State for every Skill
     local state_stab = State.new(NAMESPACE, skill_stab.identifier)
     local state_raspberry = State.new(NAMESPACE, skill_raspberry.identifier)
     local state_spoiled = State.new(NAMESPACE, skill_spoiled.identifier)
@@ -142,47 +151,74 @@ local initialize = function()
     ]]--
 
     -- Setup the Primary skill sprite, as well damage and cooldown properties
-    skill_stab:set_skill_icon(sprSkills, 0)
-    skill_stab:set_skill_properties(1, 20)
+    skill_stab:set_skill_icon(sprSkills, 0) 
+    skill_stab:set_skill_properties(1, 20) -- 100% of base damage, 20 frames cooldown
 
-    -- Called when the player tries to use its primary skill
+    -- the onActivate callback is called when the player tries to use its primary skill
     skill_stab:onActivate(function(actor, skill, index)
         GM.actor_set_state(actor, state_stab)
     end)
 
-    -- Reset the sprite animation to frame 0
+    -- Reset the sprite animation to frame 0 when you enter the state
     state_stab:onEnter(function(actor, data)
         actor.image_index = 0
         data.fired = 0
     end)
     
-    -- Implement the actual mechanics of the skill
-    state_stab:onStep(function(actor, data)
-        actor:skill_util_fix_hspeed()
+-- Implement the actual mechanics of the skill inside of the state
+state_stab:onStep(function(actor, data)
+    -- Fix the horizontal speed of the survivor (maybe when it switches states?)
+    actor:skill_util_fix_hspeed()
+    
+    -- Get the animation from the skill and set it for this state with a custom speed
+    actor:actor_animation_set(actor:actor_get_skill_animation(skill_stab), 0.25)
+    
+    -- Check if we already fired and 
+    -- if the animation is frame number is greater or equal to 4
+    -- Used to fire only once per activation and at a specific frame
+    if data.fired == 0 and actor.image_index >= 4.0 then
+
+        -- Get the damage coeff from the skill
+        local damage = actor:skill_get_damage(skill_stab)
         
-        actor:actor_animation_set(actor:actor_get_skill_animation(skill_stab), 0.25)
-        
-        if data.fired == 0 and actor.image_index >= 4.0 then
-            local damage = actor:skill_get_damage(skill_stab)
-            
-            if actor:is_authority() then
-                if not actor:skill_util_update_heaven_cracker(actor, damage) then
-                    local buff_shadow_clone = Buff.find("ror", "shadowClone")
-                    for i=0, GM.get_buff_stack(actor, buff_shadow_clone) do
-                        local attack = GM._mod_attack_fire_explosion(actor, actor.x + gm.cos(gm.degtorad(actor:skill_util_facing_direction())) * 25, actor.y, 40, 60, damage, -1, gm.constants.sSparks7)
-                        attack.max_hit_number = 5
-                        attack.attack_info.climb = i * 8
-                    end
+        -- Check if the actor is the host (sometimes used if we only want the host to trigger it)
+        -- (mostly for networked things)
+        if actor:is_authority() then
+
+            -- Check if we are not firing the heaven cracker
+            if not actor:skill_util_update_heaven_cracker(actor, damage) then
+
+                -- Get the shattered mirror buff
+                local buff_shadow_clone = Buff.find("ror", "shadowClone")
+
+                -- Fire an attack for each clone of the survivor (from shattered mirror)
+                for i=0, GM.get_buff_stack(actor, buff_shadow_clone) do
+
+                    -- Fire an explosion from the survivor
+                    local attack = GM._mod_attack_fire_explosion(actor, actor.x + gm.cos(gm.degtorad(actor:skill_util_facing_direction())) * 25, actor.y, 40, 60, damage, -1, gm.constants.sSparks7)
+
+                    -- You can also use actor:fire_explosion() but it returns a damager (attack-info) instead of an Attack object so I'm not using it for now
+                    
+                    -- Specify how many enemies should be hit by the explosion
+                    attack.max_hit_number = 5
+
+                    -- Offset the displayed damage number for each clone
+                    attack.attack_info.climb = i * 8
                 end
             end
-
-            actor:sound_play(gm.constants.wClayShoot1, 1, 0.8 + math.random() * 0.2)
-            data.fired = 1
         end
 
-        actor:skill_util_exit_state_on_anim_end()
-    end)
+        -- Play a sound at the player's location
+        -- (sound_id, volume, pitch)
+        actor:sound_play(gm.constants.wClayShoot1, 1, 0.8 + math.random() * 0.2)
 
+        -- Tell that we fired
+        data.fired = 1
+    end
+
+    -- Auto exit the state when the animation is finished
+    actor:skill_util_exit_state_on_anim_end()
+end)
     --[[
         Subsection Secondary Skill 
     ]]--
@@ -205,13 +241,20 @@ local initialize = function()
     
     -- Implement the actual mechanics of the skill inside of the state, 
     state_raspberry:onStep(function(actor, data)
+        -- Fix the horizontal speed of the survivor (maybe when it switches states?)
         actor:skill_util_fix_hspeed()
         
+        -- Get the animation from the skill and set it for this state with a custom speed
         actor:actor_animation_set(actor:actor_get_skill_animation(skill_raspberry), 0.25)
 
+        -- Check if we already fired and 
+        -- if the animation is frame number is greater or equal to 4
+        -- Used to fire only once per activation and at a specific frame
         if data.fired == 0 and actor.image_index >= 1.0 then
             local damage = actor:skill_get_damage(skill_raspberry)
             
+            -- Check if the actor is the host (sometimes used if we only want the host to trigger it)
+            -- (mostly for networked things)
             if actor:is_authority() then
                 local buff_shadow_clone = Buff.find("ror", "shadowClone")
                 for i=0, GM.get_buff_stack(actor, buff_shadow_clone) do
@@ -242,7 +285,7 @@ local initialize = function()
     end)
     
     --[[
-    Subsection Secondary Alt Skill 
+        Subsection Secondary Alt Skill 
     ]]--
     
     -- Setup the Secondary alt skill sprite, as well damage and cooldown properties
@@ -430,7 +473,7 @@ local initialize = function()
 
             -- Layer sound effects when scepter is active
             actor:sound_play(gm.constants.wGuardDeath, 0.6, 1.2 + math.random() * 0.3)            
-            actor:sound_play(gm.constants.wBoss1Shoot1, 1, 1.2 + math.random() * 0.3)  
+            actor:sound_play(gm.constants.wBoss1Shoot1, 1, 1.2 + math.random() * 0.3)
             data.spikes = data.spikes - 1          
         end
         
@@ -438,6 +481,7 @@ local initialize = function()
     end)
 end
 
+-- use this code to hot_reload the mod
 if hot_reloading then
     initialize()
 else 
