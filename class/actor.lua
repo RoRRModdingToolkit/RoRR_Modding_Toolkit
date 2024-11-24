da6650f74c96f046fb837ee35a6a38c3c924a452
Klehrik <78520710+Klehrik@users.noreply.github.com>
2024-11-24 12:31:40 -0500

    Update actor.lua

--- class/actor.lua
+++ class/actor.lua
@@ -461,13 +461,17 @@ gm.pre_script_hook(gm.constants.recalculate_stats, function(self, other, result,
     local actor = Instance.wrap(self)
     local actorData = actor:get_data()
     actorData.current_shield = actor.shield
+    actorData.current_maxshield = actor.maxshield
+
+    log.info(actorData.current_shield)
+    log.info(actorData.current_maxshield)
 end)
 
 
 gm.post_script_hook(gm.constants.recalculate_stats, function(self, other, result, args)
     local actor = Instance.wrap(self)
     local actorData = actor:get_data()
-    actor.shield = actorData.current_shield
+    actor.shield = actorData.current_shield + (actor.maxshield - actorData.current_maxshield)
     actorData.post_stat_recalc = true
 
     if not callbacks["onStatRecalc"] then return end
